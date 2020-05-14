9e801f208e85bdf064399f3a36884f6b9f5dbe99
Jinhyung Seo <dear.jinhyung@gmail.com>
2020-05-14 13:27:33 +1000

    confirm dialog

--- travel_mate/lib/components/mate_list.dart
+++ travel_mate/lib/components/mate_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelmate/models/mate_item.dart';
 import '../models/mate_data.dart';
 import 'package:provider/provider.dart';
 
@@ -9,21 +10,55 @@ class MateList extends StatelessWidget {
       builder: (context, data, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            var item = data.items[index];
             return Card(
               margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
               child: ListTile(
                 leading: Text(
-                  data.items[index].name,
+                  item.name,
                   style: TextStyle(
                     fontFamily: 'GloriaHallelujah',
                     fontSize: 25,
                   ),
                 ),
-                trailing: Text(data.items[index].price.toString()),
+                trailing: Text(item.price.toString()),
+                onLongPress: () {
+                  confirmOK(context, item);
+//                  data.removeItem(item)
+                },
               ),
             );
           },
           itemCount: data.itemCount,
+        );
+      },
+    );
+  }
+
+  confirmOK(BuildContext context, MateItem item) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text('Do you really want to delete ?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Provider.of<MateData>(context, listen: false).removeItem(item);
+                print('OK');
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+            FlatButton(
+              onPressed: () {
+                print('Cancel');
+                Navigator.pop(context);
+              },
+              child: Text('Canel'),
+            ),
+          ],
         );
       },
     );
