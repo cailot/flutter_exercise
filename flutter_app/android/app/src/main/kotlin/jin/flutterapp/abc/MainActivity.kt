396c12021a2133cde9c406bede29e79a6ca277cb
Jinhyung Seo <dear.jinhyung@gmail.com>
2020-06-10 01:35:35 +1000

    12. material design (abc)

---
+++ flutter_app/android/app/src/main/kotlin/jin/flutterapp/abc/MainActivity.kt
@@ -0,0 +1,6 @@
+package jin.flutterapp.abc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          'Jin',
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
+}
